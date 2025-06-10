626cf32595d15062d20938a75c302e23ad62d7ae
Daoud-99 <daviddaoud982@gmail.com>
2025-06-11 00:03:45 +0200

    Deuxieme commit

--- src/test/steps/BankSelectSteps.ts
+++ src/test/steps/BankSelectSteps.ts
@@ -2,14 +2,19 @@ import { Given, When, Then, setDefaultTimeout } from "@cucumber/cucumber";
 
 import { expect } from "@playwright/test";
 
-import { BankSelectPage } from  '../../pages/BankSelectPage';
+
+
 
 // Lancement et navigation vers Application
 
 Given('User navigates to the application', async function () {
-  this.bankPage = new BankSelectPage(this.page);
-  await this.bankPage.navigate();
-  await this.bankPage.acceptConsentAndOpenBankList();
+  await this.page.goto('https://connect.algoan.com/v2/init?client_id=030d0c7dfcfdcfcc135c6cf5&redirect_uri=https://dashboard.algoan.com');
+  const checkbox = this.page.locator('.sc-frniUE.fILdiP.sc-hsfCcR.hJTuhR');
+  await checkbox.click();
+
+  const bankchoiceButt = this.page.locator('.sc-giBObj.fsPiEc');
+  bankchoiceButt.click();
+
 });
 
 setDefaultTimeout(60 * 1000 * 2)
@@ -19,21 +24,24 @@ setDefaultTimeout(60 * 1000 * 2)
 
 
 Given('User enter {string} on search bar', async function (bankName: string) {
-  await this.bankPage.searchBank(bankName);
+
+  const searchInput = this.page.locator('.search-input__Input-sc-9ef27473-2.iqalfA');
+
+  await searchInput.fill(bankName);
 });
 
 
 // Scenario result 1 + 2
 
-Then('The bank {string} should be in suggestions list', async function (bankName: string) {
-  const isVisible = await this.bankPage.isBankVisible(bankName);
-  expect(isVisible).toBeTruthy();
+Then('The bank {string} should be in suggestions list', async function (bankName) {
+  const bank = await this.page.locator('.sc-bqvdXA.kjaCKu', { hasText: 'BNP Paribas' });
+  expect (bank).toBeVisible();
 });
 
 // Scenario result 3 + 4
 Then('The bank should not be in suggestions list', async function () {
-  const isHidden = await this.bankPage.isSuggestionHidden();
-  expect(isHidden).toBeTruthy();
+  const SuggestionList = this.page.locator('.sc-geoRQH.iQExIK', { hasText: 'Suggestions' });
+  expect(SuggestionList).toBeHidden();
 });
 
 
